98e2acc024f506ab82b1f8ad7385b19c4ddc072f
gaberj1 <pereirasilva169@outlook.com>
2025-09-24 11:29:44 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -14,11 +14,11 @@ function imc(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
             if (imc < 18.5 ) this.classificacao = "magreza";
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudÃ¡vel"
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudável"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
-            if (imc >= 40 ) this.classificacao = "obesidade mÃ³rbida"
+            if (imc >= 40 ) this.classificacao = "obesidade mórbida"
             return imc.toFixed(2);
         
             
@@ -29,5 +29,5 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificaÃ§Ã£o de peso Ã© " + paciente.classificacao;
-}
+    resposta.innerHTML="Seu IMC "+paciente.calcularIMC()+". " + "Sua classificação de peso é " + paciente.classificacao;
+
